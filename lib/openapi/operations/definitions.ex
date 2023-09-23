b1793d693184b3a356aa25116ba022f39c28fc54
Pablo Grant <grantpablo4@gmail.com>
2023-09-23 18:47:49 -0500

    add function definitions

---
+++ lib/openapi/operations/definitions.ex
@@ -0,0 +1,30 @@
+defmodule Pier.OpenApi.Operations.Definitions do
+  @moduledoc false
+  alias Pier.OpenApi.Operations
+  use Pier.CustomLogger, name: __MODULE__
+  defstruct [:name, :description, :required_params, :properties]
+  def run(%{contents: contents} = blueprint, _opts) do
+      definitions = Enum.map(contents["definitions"],&retrieve_metadata(&1))
+      {:ok, %{blueprint | definitions: definitions}}
+  end
+
+  def retrieve_metadata({name, spec}) do
+    Logger.metadata(module_name: __MODULE__)
+    Logger.debug("retrieving metadata for type definition: #{name}")
+      %Operations.Definitions{
+        name: name,
+        description: spec["description"],
+        required_params: spec["required"],
+        properties: get_properties(spec)
+      }
+  end
+
+  defp get_properties(%{"properties" => properties}) do
+    Enum.map(properties, fn {x, v} -> %{name: x, nullable: v["x-nullable"], type: v["type"]} end)
+
+  end
+
+  defp get_properties(_) do
+    nil
+  end
+end
